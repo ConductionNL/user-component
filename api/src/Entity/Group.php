1679ca7a774acfaf1f2d37a54c713f0e67bb139f
Gino <ginokok1996@hotmail.com>
2020-08-20 13:31:04 +0200

    organization as url

--- api/src/Entity/Group.php
+++ api/src/Entity/Group.php
@@ -77,16 +77,13 @@ class Group
     private $id;
 
     /**
-     * @var string The RSIN of the organization that owns this group
+     * @var string A specific commonground organization
      *
-     * @example 002851234
+     * @example https://wrc.zaakonline.nl/organisations/16353702-4614-42ff-92af-7dd11c8eef9f
      *
-     * @Gedmo\Versioned
      * @Assert\NotNull
-     * @Assert\Length(
-     *      min = 8,
-     *      max = 11
-     * )
+     * @Assert\Url
+     * @Gedmo\Versioned
      * @Groups({"read", "write"})
      * @ORM\Column(type="string", length=255)
      */
