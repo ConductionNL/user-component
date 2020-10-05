45b5eda3686fcd64ba9d51d1999ff83e07664c84
ginokok1996 <ginokok1996@hotmail.com>
2020-10-05 14:01:22 +0200

    renamed reset provider to token provider

--- api/src/DataFixtures/CheckinFixtures.php
+++ api/src/DataFixtures/CheckinFixtures.php
@@ -144,9 +144,9 @@ class CheckinFixtures extends Fixture
         $manager->persist($provider);
 
         $provider = new Provider();
-        $provider->setName('reset');
-        $provider->setDescription('provider for resetting password');
-        $provider->setType('reset');
+        $provider->setName('token');
+        $provider->setDescription('provider for one time tokens');
+        $provider->setType('token');
         $provider->setOrganization($this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'organizations', 'id'=>'4d1eded3-fbdf-438f-9536-8747dd8ab591']));
         $provider->setApplication($this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'applications', 'id'=>'31a2ad29-ee03-4aa9-be81-abf1fda7bbcc']));
         $manager->persist($provider);
