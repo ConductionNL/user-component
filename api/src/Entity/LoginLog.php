1b0cc9253aa9fdeba107eb89bd0961eb8570c395
ginokok1996 <ginokok1996@hotmail.com>
2020-10-15 10:34:37 +0200

    loginLog object added + modelio and postman

---
+++ api/src/Entity/LoginLog.php
@@ -0,0 +1,246 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LoginLogRepository;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use DateTime;
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * An entity representing an login log.
+ *
+ * This entity logs all the login attempts
+ *
+ * @author Gino Kok <gino@conduction.nl>
+ *
+ * @category entity
+ *
+ * @license EUPL <https://github.com/ConductionNL/Proto-application-NLDesign/blob/master/LICENSE.md>
+ *
+ * @ApiResource(
+ *     normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
+ *     denormalizationContext={"groups"={"write"}, "enable_max_depth"=true},
+ *     itemOperations={
+ *          "get",
+ *          "put",
+ *          "delete",
+ *          "get_change_logs"={
+ *              "path"="/login_logs/{id}/change_log",
+ *              "method"="get",
+ *              "swagger_context" = {
+ *                  "summary"="Changelogs",
+ *                  "description"="Gets al the change logs for this resource"
+ *              }
+ *          },
+ *          "get_audit_trail"={
+ *              "path"="/login_logs/{id}/audit_trail",
+ *              "method"="get",
+ *              "swagger_context" = {
+ *                  "summary"="Audittrail",
+ *                  "description"="Gets the audit trail for this resource"
+ *              }
+ *          }
+ *     },
+ *     collectionOperations={
+ *          "get",
+ *          "post"
+ *     }
+ * )
+ * @ORM\Entity(repositoryClass=LoginLogRepository::class)
+ * @Gedmo\Loggable(logEntryClass="Conduction\CommonGroundBundle\Entity\ChangeLog")
+ * @ORM\HasLifecycleCallbacks
+ *
+ * @ApiFilter(OrderFilter::class)
+ * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
+ * @ApiFilter(SearchFilter::class, properties={ "address": "exact", "method": "exact", "status": "exact" })
+ */
+class LoginLog
+
+{
+    /**
+     * @var UuidInterface The UUID identifier of this object
+     *
+     * @example e2984465-190a-4562-829e-a8cca81aa35d
+     *
+     *
+     * @Groups({"read"})
+     * @Assert\Uuid
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    private $id;
+
+    /**
+     * @var string The ip address of the login
+     *
+     * @Gedmo\Versioned
+     *
+     * @example 12.12.12.123
+     * @Groups({"read","write"})
+     * @Assert\NotNull
+     * @Assert\Length(
+     *     max=50
+     * )
+     * @ORM\Column(type="string", length=50)
+     */
+    private $address;
+
+    /**
+     * @var string The method of the login
+     *
+     * @Gedmo\Versioned
+     *
+     * @example facebook
+     * @Groups({"read","write"})
+     * @Assert\NotNull
+     * @Assert\Length(
+     *     max=255
+     * )
+     * @ORM\Column(type="string", length=255)
+     */
+    private $method;
+
+    /**
+     * @var string The application uri used for this login
+     *
+     * @Gedmo\Versioned
+     *
+     * @example facebook
+     * @Groups({"read","write"})
+     * @Assert\NotNull
+     * @Assert\Url
+     * @Assert\Length(
+     *     max=255
+     * )
+     * @ORM\Column(type="string", length=255)
+     */
+    private $application;
+
+    /**
+     * @var string the status code of the login
+     *
+     * @Gedmo\Versioned
+     *
+     * @example 200
+     * @Groups({"read","write"})
+     * @Assert\NotNull
+     * @Assert\Length(
+     *     max=3
+     * )
+     * @ORM\Column(type="string", length=3)
+     */
+    private $status;
+
+    /**
+     * @var DateTime The moment this login was made
+     *
+     * @example 20190101
+     *
+     * @Groups({"read"})
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateCreated;
+
+    /**
+     * @var DateTime The moment this login was updated
+     *
+     * @example 20190101
+     *
+     * @Groups({"read"})
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateModified;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getMethod(): ?string
+    {
+        return $this->method;
+    }
+
+    public function setMethod(string $method): self
+    {
+        $this->method = $method;
+
+        return $this;
+    }
+
+    public function getApplication(): ?string
+    {
+        return $this->application;
+    }
+
+    public function setApplication(string $application): self
+    {
+        $this->application = $application;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function setDateCreated(DateTimeInterface $dateCreated): self
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    public function getDateModified(): ?DateTimeInterface
+    {
+        return $this->dateModified;
+    }
+
+    public function setDateModified(DateTimeInterface $dateModified): self
+    {
+        $this->dateModified = $dateModified;
+
+        return $this;
+    }
+}
