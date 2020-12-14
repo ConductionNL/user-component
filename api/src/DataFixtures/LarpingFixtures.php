d0285704eac9800c9e7d76c8df70cb0efe7b27cf
Barry Brands <barrybrands02@hotmail.com>
2020-12-14 16:25:16 +0100

    larpingfixtures for id-vault

--- api/src/DataFixtures/LarpingFixtures.php
+++ api/src/DataFixtures/LarpingFixtures.php
@@ -29,38 +29,20 @@ class LarpingFixtures extends Fixture
         // Lets make sure we only run these fixtures on larping enviroment
         if (
             !$this->params->get('app_build_all_fixtures') &&
-            $this->params->get('app_domain') != 'zuid-drecht.nl' && strpos($this->params->get('app_domain'), 'zuid-drecht.nl') == false &&
             $this->params->get('app_domain') != 'larping.eu' && strpos($this->params->get('app_domain'), 'larping.eu') == false
         ) {
             return false;
         }
 
-        $provider = new Provider();
-        $provider->setName('facebook');
-        $provider->setDescription('facebook');
-        $provider->setType('facebook');
-        $provider->setOrganization($this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'organizations', 'id'=>'4d1eded3-fbdf-438f-9536-8747dd8ab591']));
-        $provider->setApplication($this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'applications', 'id'=>'31a2ad29-ee03-4aa9-be81-abf1fda7bbcc']));
-        $provider->setConfiguration(['app_id'=>str_replace('\'', '', $this->params->get('facebook_id')), 'secret'=>$this->params->get('facebook_secret')]);
-        $manager->persist($provider);
-
-        $provider = new Provider();
-        $provider->setName('gmail');
-        $provider->setDescription('gmail');
-        $provider->setType('gmail');
-        $provider->setOrganization($this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'organizations', 'id'=>'7b863976-0fc3-4f49-a4f7-0bf7d2f2f535']));
-        $provider->setApplication($this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'applications', 'id'=>'9798cae6-187a-434f-bd66-f1dc2cc61466']));
-        $provider->setConfiguration(['app_id'=>$this->params->get('gmail_id'), 'secret'=>$this->params->get('gmail_secret')]);
-        $manager->persist($provider);
-
         // Larping provider
+
         $provider = new Provider();
-        $provider->setName('id-vault');
+        $provider->setName('id-vault larping');
         $provider->setDescription('id-vault provider');
         $provider->setType('id-vault');
-        $provider->setOrganization($this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'organizations', 'id' => '7b863976-0fc3-4f49-a4f7-0bf7d2f2f535']));
-        $provider->setApplication($this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'applications', 'id' => '9798cae6-187a-434f-bd66-f1dc2cc61466']));
-        $provider->setConfiguration(['app_id'=>'c243189f-11cf-40c5-8859-6a57555328bf', 'secret'=>'eeb22abf509d45c59ddf97c8c39b67ae']);
+        $provider->setOrganization($this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'organizations', 'id'=>'7b863976-0fc3-4f49-a4f7-0bf7d2f2f535']));
+        $provider->setApplication($this->commonGroundService->cleanUrl(['component'=>'wrc', 'type'=>'applications', 'id'=>'9798cae6-187a-434f-bd66-f1dc2cc61466']));
+        $provider->setConfiguration(['app_id'=>'5af1e409-3d49-45f9-871b-6e4e35b6f5fc', 'secret'=>'bd4f6884-7819-444f-9968-5f5565029080']);
         $manager->persist($provider);
 
         $manager->flush();
@@ -70,97 +52,8 @@ class LarpingFixtures extends Fixture
         $user->setUsername('test@larping.eu');
         $user->setPassword($this->encoder->encodePassword($user, 'bierflesje'));
         $manager->persist($user);
-//
-//        // Larping
-//        $groupUsers = new Group();
-//        $groupUsers->setName('Users');
-//        $groupUsers->setDescription('The Users of Vortex Larping');
-//        $groupUsers->setOrganization('https://wrc.larping.eu/organizations/organizations/39405560-7859-4d16-943b-042d6c053a0f'); // Larping
-//        $groupUsers->addUser($user);
-//        $manager->persist($groupUsers);
-//
-//        $groupAdmin = new Group();
-//        $groupAdmin->setName('Admin');
-//        $groupAdmin->setDescription('The Administrators of Larping');
-//        $groupAdmin->setParent($groupUsers);
-//        $groupAdmin->setOrganization('https://wrc.larping.eu/organizations/organizations/39405560-7859-4d16-943b-042d6c053a0f'); // Larping
-//        $groupAdmin->addUser($user);
-//        $manager->persist($groupAdmin);
-//
-//        // Vortex Adventures
-//
-//        $groupUsers = new Group();
-//        $groupUsers->setName('Users');
-//        $groupUsers->setDescription('The Users of Vortex Adventures');
-//        $groupUsers->setOrganization('https://wrc.larping.eu/organizations/0972a00f-1893-4e9b-ac13-0e43f225eca5'); //VA
-//        $manager->persist($groupUsers);
-//
-//        $groupAdmin = new Group();
-//        $groupAdmin->setName('Admin');
-//        $groupAdmin->setDescription('The Administrators of Vortex Adventures');
-//        $groupAdmin->setParent($groupUsers);
-//        $groupAdmin->setOrganization('https://wrc.larping.eu/organizations/0972a00f-1893-4e9b-ac13-0e43f225eca5'); //VA
-//        $groupAdmin->addUser($user);
-//        $manager->persist($groupAdmin);
-//
-//        $groupMember = new Group();
-//        $groupMember->setName('Member');
-//        $groupMember->setDescription('The Members of Vortex Adventures');
-//        $groupMember->setParent($groupUsers);
-//        $groupMember->setOrganization('https://wrc.larping.eu/organizations/0972a00f-1893-4e9b-ac13-0e43f225eca5'); //VA
-//        $manager->persist($groupMember);
-//
-//        $groupCrew = new Group();
-//        $groupCrew->setName('Crew');
-//        $groupCrew->setDescription('The Crew members of Vortex Adventures');
-//        $groupCrew->setParent($groupUsers);
-//        $groupCrew->setOrganization('https://wrc.larping.eu/organizations/0972a00f-1893-4e9b-ac13-0e43f225eca5'); //VA
-//        $manager->persist($groupCrew);
-//
-//        $groupGameMasters = new Group();
-//        $groupGameMasters->setName('Game Masters');
-//        $groupGameMasters->setDescription('The Game Masters of Vortex Adventures');
-//        $groupGameMasters->setParent($groupCrew);
-//        $groupGameMasters->setOrganization('https://wrc.larping.eu/organizations/0972a00f-1893-4e9b-ac13-0e43f225eca5'); //VA
-//        $manager->persist($groupGameMasters);
-//
-//        $groupVolunteers = new Group();
-//        $groupVolunteers->setName('Volunteers');
-//        $groupVolunteers->setDescription('The Volunteers of Vortex Adventures');
-//        $groupVolunteers->setParent($groupCrew);
-//        $groupVolunteers->setOrganization('https://wrc.larping.eu/organizations/0972a00f-1893-4e9b-ac13-0e43f225eca5'); //VA
-//        $manager->persist($groupVolunteers);
-//
-//        $groupExtras = new Group();
-//        $groupExtras->setName('Extras');
-//        $groupExtras->setDescription('The Extra\'s or npc\'s of Vortex Adventures');
-//        $groupExtras->setParent($groupCrew);
-//        $groupExtras->setOrganization('https://wrc.larping.eu/organizations/0972a00f-1893-4e9b-ac13-0e43f225eca5'); //VA
-//        $manager->persist($groupExtras);
-//
-//        $groupBoard = new Group();
-//        $groupBoard->setName('Board');
-//        $groupBoard->setDescription('The Board members of Vortex Adventures');
-//        $groupBoard->setParent($groupCrew);
-//        $groupBoard->setOrganization('https://wrc.larping.eu/organizations/0972a00f-1893-4e9b-ac13-0e43f225eca5'); //VA
-//        $manager->persist($groupBoard);
-//
-//        $user = new User();
-//        $user->setOrganization('https://wrc.larping.eu/organizations/0972a00f-1893-4e9b-ac13-0e43f225eca5'); // Larping
-//        $user->setUsername('info@the-vortex.nl');
-//        $user->setPassword($this->encoder->encodePassword($user, 'lampenkap1986'));
-//        /*
-//    	$user->addGroup($groupUsers);
-//    	$user->addGroup($groupMember);
-//    	$user->addGroup($groupAdmin);
-//    	$user->addGroup($groupCrew);
-//    	$user->addGroup($groupGameMasters);
-//    	$user->addGroup($groupVolunteers);
-//    	$user->addGroup($groupExtras);
-//    	$user->addGroup($groupBoard);
-//    	*/
-//        $manager->persist($user);
-//
-//        $manager->flush();
+
+        $manager->flush();
+
     }
 }
