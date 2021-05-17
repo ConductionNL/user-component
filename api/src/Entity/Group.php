241f70596e13117dc455a0eb554265f50e120f99
Sarai Misidjan <sarai@conduction.nl>
2021-05-17 15:53:14 +0200

    added filter

--- api/src/Entity/Group.php
+++ api/src/Entity/Group.php
@@ -58,7 +58,7 @@ use Symfony\Component\Validator\Constraints\DateTime;
  * @ApiFilter(BooleanFilter::class)
  * @ApiFilter(OrderFilter::class)
  * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
- * @ApiFilter(SearchFilter::class, properties={"organization":"partial", "code":"exact"})
+ * @ApiFilter(SearchFilter::class, properties={"organization":"partial", "code":"exact", "name":"exact"})
  */
 class Group
 {
