76fd25444e6953596c8d093cdda7ce4dec2214af
ginokok1996 <ginokok1996@hotmail.com>
2021-06-14 19:26:50 +0200

    another login endpoint fix

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -1,21 +1,18 @@
 <?php
 
-// src/Controller/DefaultController.php
+// api/src/Controller/BookController.php
 
 namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-/**
- * Class DefaultController.
- */
 class DefaultController extends AbstractController
 {
-
     public function __invoke(User $data): User
     {
+        //$this->bookPublishingHandler->handle($data);
+
         return $data;
     }
-
 }
