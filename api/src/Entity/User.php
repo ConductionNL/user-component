e4d6569f85fba21c2914a357f77825957c6d605a
Wilco Louwerse <wilco@conduction.nl>
2021-10-18 15:38:23 +0200

    setting id with hautelook/alice-bundle does not work yet

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -14,6 +14,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -308,14 +309,14 @@ class User implements PasswordAuthenticatedUserInterface
         $this->signingTokens = new ArrayCollection();
     }
 
-    public function getId()
+    public function getId(): UuidInterface
     {
         return $this->id;
     }
 
     public function setId(string $id): self
     {
-        $this->id = $id;
+        $this->id = Uuid::fromString($id);
 
         return $this;
     }
