d934cd530f21da20600ce038ec17e37cb06b143f
Robert Zondervan <Robert@conduction.nl>
2022-02-10 12:54:10 +0100

    Prevent doubling usernames after enabling case insensitivity

--- api/src/Subscriber/DuplicateUsernameSubscriber.php
+++ api/src/Subscriber/DuplicateUsernameSubscriber.php
@@ -5,6 +5,7 @@ namespace App\Subscriber;
 use ApiPlatform\Core\EventListener\EventPriorities;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
@@ -17,12 +18,14 @@ class DuplicateUsernameSubscriber implements EventSubscriberInterface
     private $em;
     private $encoder;
     private $request;
+    private ParameterBagInterface $parameterBag;
 
-    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $encoder)
+    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $encoder, ParameterBagInterface $parameterBag)
     {
         $this->em = $em;
         $this->encoder = $encoder;
         $this->request = Request::createFromGlobals();
+        $this->parameterBag = $parameterBag;
     }
 
     public static function getSubscribedEvents()
@@ -41,7 +44,24 @@ class DuplicateUsernameSubscriber implements EventSubscriberInterface
             return;
         }
 
-        if ($route == 'api_users_post_collection' && $newUser = $this->em->getRepository(User::class)->findOneBy(['username' => $user->getUsername()])) {
+        if (
+            $route == 'api_users_post_collection' &&
+            (!$this->parameterBag->has('case_insensitive_username') || !$this->parameterBag->get('case_insensitive_username')) &&
+            $newUser = $this->em->getRepository(User::class)->findOneBy(['username' => $user->getUsername()])
+        ) {
+            throw new HttpException(409, 'Username is unavailable');
+        } elseif (
+            $route == 'api_users_post_collection' &&
+            $this->parameterBag->has('case_insensitive_username') &&
+            $this->parameterBag->get('case_insensitive_username') &&
+            $newUser = $this->em
+                ->getRepository('App\Entity\User')
+                ->createQueryBuilder('a')
+                ->where('upper(a.username) = upper(:username)')
+                ->setParameter('username', $user->getUsername())
+                ->getQuery()
+                ->execute())
+        {
             throw new HttpException(409, 'Username is unavailable');
         } elseif ($route == 'api_users_put_item' && $event->getRequest()->get('previous_data')->getUsername() !== $event->getRequest()->get('data')->getUsername()) {
             if ($newUser = $this->em->getRepository(User::class)->findOneBy(['username' => $event->getRequest()->get('data')->getUsername()])) {
